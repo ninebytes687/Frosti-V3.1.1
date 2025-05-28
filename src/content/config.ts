e8878f1fdcf1dad6cbf8b3fe36ffdca710c313ec
ninebytes687 <127567788+ninebytes687@users.noreply.github.com>
2025-05-28 15:32:34 +0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,8 @@ const blog = defineCollection({
     image: z.string().optional(),
     badge: z.string().optional(),
     draft: z.boolean().default(false),
+    // +++ 新增评论开关字段 +++
+    enableComments: z.boolean().default(false).optional(), // 默认关闭评论
     categories: z
       .array(z.string())
       .refine((items) => new Set(items).size === items.length, {
